3d76da879818cc7d72bcfa5131e735c9dc9651aa
deepikasampangi <deepikasampangi@gmail.com>
2020-06-27 16:19:12 +0530

    variables use

---
+++ VarUsage/varUsage.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	fmt.Println("Declaring a variable")
+	var x int
+	var a, b string
+	x = 1
+	a, b = "a", "b"
+	fmt.Println("value of x is", x, "Type of x is", reflect.TypeOf(x))
+	fmt.Println("value of a is", a)
+	fmt.Println("value of b is", b)
+
+}
