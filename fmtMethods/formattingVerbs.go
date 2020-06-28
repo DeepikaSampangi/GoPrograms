340b846b7df17500bda237b361e94b630c031641
deepikasampangi <deepikasampangi@gmail.com>
2020-06-28 18:41:17 +0530

    formatting width

--- fmtMethods/formattingVerbs.go
+++ fmtMethods/formattingVerbs.go
@@ -13,4 +13,14 @@ func main() {
 	fmt.Printf("Any Vaule decalred %#v, %#v, %#v \n", 4.9, "\t", false)
 	fmt.Printf("Types of the values 1.2 : %T, tab space : %T, Bool value : %T \n", 1.2, "\t", true)
 	fmt.Printf("Percent sign : %% \n")
+
+	fmt.Println("** Formatting values width **")
+	fmt.Printf("%%15s : %15s\n", "Hello")
+	fmt.Printf("%%4d : %4d\n", 20)
+	fmt.Printf("%%7.3f : %7.3f \n", 15.6754)
+	fmt.Printf("%%1.3f : %1.3f \n", 15.6754)
+	fmt.Printf("%%7.2f : %7.2f \n", 15.6754)
+	fmt.Printf("%%7.1f : %7.1f \n", 15.6754)
+	fmt.Printf("%%.1f : %.1f \n", 15.6754)
+	fmt.Printf("%%.2f : %.2f \n", 15.6754)
 }
