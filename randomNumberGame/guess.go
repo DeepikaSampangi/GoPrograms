43e2c9a14fc5abeb41e2725737830d8157aafdf3
deepikasampangi <deepikasampangi@gmail.com>
2020-06-28 17:44:18 +0530

    random number game in go

--- randomNumberGame/guess.go
+++ randomNumberGame/guess.go
@@ -15,17 +15,18 @@ func main() {
 	seconds := time.Now().Unix()
 	rand.Seed(seconds)
 	target := rand.Intn(100) + 1
-	fmt.Println("Guess a random number between 1 and 100 :")
+	fmt.Println("Guess a random number between 1 and 100 with 10 Chances:")
+	success := false
 	for i := 0; i <= 10; i++ {
 		reader := bufio.NewReader(os.Stdin)
 		fmt.Print("please provide your guess: ")
 		input, err := reader.ReadString('\n')
-		if err != nil{
+		if err != nil {
 			log.Fatal(err)
 		}
 		input = strings.TrimSpace(input)
 		guess, err := strconv.Atoi(input)
-		if err != nil{
+		if err != nil {
 			log.Fatal(err)
 		}
 		if guess < target {
@@ -34,8 +35,12 @@ func main() {
 			fmt.Println("Oops! Your guess is high")
 		} else {
 			fmt.Println("Wow, Congratulations, you got it right !")
+			success = true
 			break
 		}
+	}
+	if !success {
+		fmt.Println("Sorry, you didnt guess it right !")
 	}
 	fmt.Println(target)
 }
