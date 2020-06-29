00b59b1baba5ebbc00be201c9d932824535d1d20
deepikasampangi <deepikasampangi@gmail.com>
2020-06-29 21:17:30 +0530

    Slices Operations two Dim Array

--- simplScripts/sliceExample.go
+++ simplScripts/sliceExample.go
@@ -15,4 +15,24 @@ func main() {
 	s[2] = "Hai"
 	fmt.Println("Elements in set are : ", s)
 	fmt.Println("Length of set", len(s))
+
+	s = append(s, "World")
+	s = append(s, "Universe", "Galaxy")
+	fmt.Println("Elements in set after appending : ", s)
+
+	sub_list := s[3:6]
+	fmt.Println("Elements in sub list are :", sub_list)
+
+	s2 := []string{"Planets", "Stars", "Comets"}
+	fmt.Println("List createad at declaration", s2)
+
+	twoDim := make([][]int, 3)
+	for i := 0; i < 3; i++ {
+		innerLen := i + 1
+		twoDim[i] = make([]int, innerLen)
+		for j := 0; j < innerLen; j++ {
+			twoDim[i][j] = i + j
+		}
+	}
+	fmt.Println("twoD list", twoDim)
 }
