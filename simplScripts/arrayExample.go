59aecbaf280543fb951777f115ae65da50b90fd5
deepikasampangi <deepikasampangi@gmail.com>
2020-07-05 15:34:54 +0530

    Array List in Go

---
+++ simplScripts/arrayExample.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Declaring an Array")
+
+	var myArr [5]string
+
+	myArr[0] = "sa"
+	myArr[1] = "re"
+	myArr[2] = "ga"
+	myArr[3] = "ma"
+	myArr[4] = "pa"
+	fmt.Println(myArr)
+
+}
